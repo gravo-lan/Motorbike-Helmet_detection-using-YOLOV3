d328f3d0d6f6e2f3d9a57a9fc7fd7da21180553a
GravoLan <66149576+gravo-lan@users.noreply.github.com>
2024-07-04 16:13:52 +0800

    Restructure file and use video

--- motorbike_helmet_detection_image.py
+++ motorbike_helmet_detection_image.py
@@ -4,105 +4,140 @@ from utils import label_map_util
 import tensorflow as tf
 from utils import visualization_utils as vis_util
 
-def detection(helmet_inference_path, frozen_graph_path, labelmap, number_of_classes, input):
+class ObjectDetector:
+    def __init__(self, helmet_inference_path, frozen_graph_path, labelmap, number_of_classes):
+        self.helmet_inference_path = helmet_inference_path
+        self.frozen_graph_path = frozen_graph_path
+        self.labelmap = labelmap
+        self.number_of_classes = number_of_classes
+        self.detection_graph = tf.Graph()
 
-    detection_graph = tf.Graph()
-    # Name of the directory containing the object detection module we're using
-    TRAINED_MODEL_DIR = helmet_inference_path
+    def detection(self, input):
 
-    # Path to frozen detection graph .pb file, which contains the model that is used
-    # for object detection.
-    PATH_TO_CKPT = TRAINED_MODEL_DIR + frozen_graph_path
-    print(PATH_TO_CKPT)
-    # Path to label map file
-    PATH_TO_LABELS = TRAINED_MODEL_DIR + labelmap
+        # Name of the directory containing the object detection module we're using
+        TRAINED_MODEL_DIR = self.helmet_inference_path
 
-    # Number of classes the object detector can identify
-    NUM_CLASSES = number_of_classes
+        # Path to frozen detection graph .pb file, which contains the model that is used
+        # for object detection.
+        PATH_TO_CKPT = TRAINED_MODEL_DIR + self.frozen_graph_path
+        print(PATH_TO_CKPT)
+        # Path to label map file
+        PATH_TO_LABELS = TRAINED_MODEL_DIR + self.labelmap
 
-    label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-    categories = label_map_util.convert_label_map_to_categories(
-        label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+        # Number of classes the object detector can identify
+        NUM_CLASSES = self.number_of_classes
 
-    category_index = label_map_util.create_category_index(categories)
+        label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+        categories = label_map_util.convert_label_map_to_categories(
+            label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 
-    # Load the Tensorflow model into memory.
+        category_index = label_map_util.create_category_index(categories)
 
-    print("> ====== Loading frozen graph into memory")
+        # Load the Tensorflow model into memory.
 
-    with detection_graph.as_default():
-        od_graph_def = tf.GraphDef()
-        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-            serialized_graph = fid.read()
-            od_graph_def.ParseFromString(serialized_graph)
-            tf.import_graph_def(od_graph_def, name='')
+        print("> ====== Loading frozen graph into memory")
 
-        sess = tf.Session(graph=detection_graph)
-        print(">  ====== Inference graph loaded.")
+        with self.detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
 
-    # Define input and output tensors (i.e. data) for the object detection classifier
+            sess = tf.Session(graph=self.detection_graph)
+            print(">  ====== Inference graph loaded.")
 
-    # Input tensor is the image
-    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+        # Define input and output tensors (i.e. data) for the object detection classifier
 
-    # Output tensors are the detection boxes, scores, and classes
-    # Each box represents a part of the image where a particular object was detected
-    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+        # Input tensor is the image
+        image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
 
-    # Each score represents level of confidence for each of the objects.
-    # The score is shown on the result image, together with the class label.
-    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
-    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+        # Output tensors are the detection boxes, scores, and classes
+        # Each box represents a part of the image where a particular object was detected
+        detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
 
-    # Number of objects detected
-    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-    img = input
+        # Each score represents level of confidence for each of the objects.
+        # The score is shown on the result image, together with the class label.
+        detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+        detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
 
-    # Load the Tensorflow model into memory.
-    # Load image using OpenCV and
-    # expand image dimensions to have shape: [1, None, None, 3]
-    # i.e. a single-column array, where each item in the column has the pixel RGB value
-    sess = tf.Session(graph=detection_graph)
-    image = cv2.imread(img)
-    image = cv2.resize(image, (1080, 1080))
-    image_expanded = np.expand_dims(image, axis=0)
-    # Perform the actual detection by running the model with the image as input
-    (boxes, scores, classes, num) = sess.run(
-        [detection_boxes, detection_scores, detection_classes, num_detections],
-        feed_dict={image_tensor: image_expanded})
+        # Number of objects detected
+        num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
+        img = input
 
-    return category_index,image, boxes, scores, classes, num
+        # Load the Tensorflow model into memory.
+        # Load image using OpenCV and
+        # expand image dimensions to have shape: [1, None, None, 3]
+        # i.e. a single-column array, where each item in the column has the pixel RGB value
+        sess = tf.Session(graph=self.detection_graph)
+        image = cv2.imread(img)
+        image = cv2.resize(image, (1080, 1080))
+        image_expanded = np.expand_dims(image, axis=0)
+        # Perform the actual detection by running the model with the image as input
+        (boxes, scores, classes, num) = sess.run(
+            [detection_boxes, detection_scores, detection_classes, num_detections],
+            feed_dict={image_tensor: image_expanded})
 
-# Get the category index, images, boxes, scores and classes for helmet
+        return category_index,image, boxes, scores, classes, num
 
-category_index_helmet, image_helmet, boxes_helmet, scores_helmet, classes_helmet, num_helmet = \
-    detection('frozen_graphs', '/frozen_inference_graph_helmet.pb', '/labelmap_helmet.pbtxt', 2, 'image10.jpg')
 
-# Get the category index, images, boxes, scores and classes for motorbike and person
+def visualise(frame, boxes, scores, classes, category_index):
+    vis_util.visualize_boxes_and_labels_on_image_array(
+            frame,
+            np.squeeze(boxes),
+            np.squeeze(classes).astype(np.int32),
+            np.squeeze(scores),
+            category_index,
+            use_normalized_coordinates=True,
+            line_thickness=6,
+            min_score_thresh=0.70)
 
-category_index_motorbike, image_helmet, boxes_motorbike, scores_motorbike, classes_motorbike, num_motorbike = \
-    detection('frozen_graphs', '/frozen_inference_graph_motorbike.pb', '/labelmap_motorbike.pbtxt', 4, 'image10.jpg')
 
-vis_util.visualize_boxes_and_labels_on_image_array(
-        image_helmet,
-        np.squeeze(boxes_motorbike),
-        np.squeeze(classes_motorbike).astype(np.int32),
-        np.squeeze(scores_motorbike),
-        category_index_motorbike,
-        use_normalized_coordinates=True,
-        line_thickness=6,
-        min_score_thresh=0.75)
+def create_video_writer(video_cap, output_filename):
 
-k=vis_util.visualize_boxes_and_labels_on_image_array(
-        image_helmet,
-        np.squeeze(boxes_helmet),
-        np.squeeze(classes_helmet).astype(np.int32),
-        np.squeeze(scores_helmet),
-        category_index_helmet,
-        use_normalized_coordinates=True,
-        line_thickness=6,
-        min_score_thresh=0.70)
-cv2.imshow('Object detector', k)
-cv2.imwrite('helmet_bike_detection_10.jpg', k)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # grab the width, height, and fps of the frames in the video stream.
+    frame_width = int(video_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(video_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(video_cap.get(cv2.CAP_PROP_FPS))
+
+    # initialize the FourCC and a video writer object
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    writer = cv2.VideoWriter(output_filename, fourcc, fps,
+                             (frame_width, frame_height))
+
+    return writer
+
+
+def main():
+    # Initialize video capture from webcam (change path if using a video file)
+    cap = cv2.VideoCapture(0)  # Use 0 for default webcam
+    detector = ObjectDetector('frozen_graphs', '/frozen_inference_graph_helmet.pb','/labelmap_helmet.pbtxt', 2)
+    writer = create_video_writer(cap, "output.mp4")
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break  # End of video stream
+
+        # Perform object detection on the current frame
+        category_index_helmet, _, boxes_helmet, scores_helmet, classes_helmet = detector.detection(frame)
+
+        # Visualize detected objects on the frame
+        visualise(frame, boxes_helmet, scores_helmet, classes_helmet, category_index_helmet)
+
+        # Display the frame
+        cv2.namedWindow("output", cv2.WINDOW_NORMAL)
+        cv2.imshow('Object detector', frame)
+        writer.write(frame)
+
+        # Press 'q' to exit
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release video capture and close all windows
+    cap.release()
+    writer.release
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
